46cf2a07388a6ec09703c3ec179cc1f5f9c9d622
okasha <okashoon@gmail.com>
2017-02-07 13:06:15 +0200

    changing positions

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
+
+window.onload = function(){
 var map = [
-[0,0,2,0,1,3],
-[1,0,0,2,0,1],
-[2,0,2,0,1,1],
-[0,0,0,2,0,0],
-[2,0,0,1,0,2],
-[1,0,0,2,0,0]
+[0, 0, 2, 0, 1, 3],
+[1, 0, 0, 2, 0, 1],
+[2, 0, 2, 0, 1, 1],
+[0, 0, 0, 2, 0, 0],
+[2, 0, 0, 1, 0, 2],
+[1, 0, 0, 2, 0, 0]
 ];
 
 var gameObjects = [
 [0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 5, 0],
 [0, 0, 0, 0, 0, 0],
 [4, 0, 0, 0, 0, 0]
 ];
@@ -26,6 +28,8 @@ var ISLAND = 1;
 var PIRATE = 2;
 var HOME = 3;
 var SHIP = 4;
+var MONSTER = 5;
+var WRECK = 6;
 
 var SIZE = 70;
 
@@ -34,6 +38,9 @@ var COLUMNS = map[0].length;
 
 var shipRow;
 var shipColumn;
+
+var monsterRow;
+var monsterColumn;
 
 var food = 10;
 var gold = 10;
@@ -45,6 +52,10 @@ for(var i = 0; i < gameObjects.length; i++){
 		if(gameObjects[i][j] === SHIP){
 			shipRow = i;
 			shipColumn = j;
+		}
+		if(gameObjects[i][j] === MONSTER){
+			monsterRow = i;
+			monsterColumn = j;
 		}
 	}
 }
@@ -59,7 +70,7 @@ document.addEventListener("keydown", keydownHandler, false);
 render();
 
 function render(){
-
+	console.log("render");
 	if(stage.hasChildNodes()){
 		for(var i = 0; i< ROWS*COLUMNS;i++){
 			stage.removeChild(stage.firstChild);
@@ -95,14 +106,21 @@ function render(){
 			if(gameObjects[row][column] === SHIP){
 				cell.src = "images/ship.png";
 			}
+			if(gameObjects[row][column] === MONSTER){
+				cell.src = "images/monster.png";
+			}
+			if(gameObjects[row][column] === WRECK){
+				console.log("image wreck");
+				cell.src = "images/wreck.png";
+			}
 			
 		}
 	}
 	output.innerHTML = gameMessage;
 
-output.innerHTML
-+= "<br>Gold: " + gold + ", Food: "
-+ food + ", Experience: " + experience;
+	output.innerHTML
+	+= "<br>Gold: " + gold + ", Food: "
+	+ food + ", Experience: " + experience;
 }
 
 function keydownHandler(event){
@@ -111,28 +129,44 @@ function keydownHandler(event){
 		if(shipRow>0){
 			gameObjects[shipRow][shipColumn] = 0;
 			shipRow--;
+			if(gameObjects[shipRow][shipColumn] === MONSTER){
+				endGame();
+			} else {
 			gameObjects[shipRow][shipColumn] = SHIP;
+			}
 		}
 		break;
 		case DOWN:
 		if(shipRow<ROWS-1){
 			gameObjects[shipRow][shipColumn] = 0;
 			shipRow++;
+			if(gameObjects[shipRow][shipColumn] === MONSTER){
+				endGame();
+			} else {
 			gameObjects[shipRow][shipColumn] = SHIP;
+			}
 		}
 		break;
 		case RIGHT:
 		if(shipColumn<COLUMNS-1){
 			gameObjects[shipRow][shipColumn] = 0;
 			shipColumn++;
+			if(gameObjects[shipRow][shipColumn] === MONSTER){
+				endGame();
+			} else {
 			gameObjects[shipRow][shipColumn] = SHIP;
+			}
 		}
 		break;
 		case LEFT:
 		if(shipColumn>0){
 			gameObjects[shipRow][shipColumn] = 0;
 			shipColumn--;
+			if(gameObjects[shipRow][shipColumn] === MONSTER){
+				endGame();
+			} else {
 			gameObjects[shipRow][shipColumn] = SHIP;
+			}
 		}
 		break;
 
@@ -164,12 +198,29 @@ function keydownHandler(event){
 		endGame();
 	}
 
-
-
-
+	
+	console.log( shipRow + " : " + shipColumn);
+	console.log( gameObjects[shipRow][shipColumn] );
+	console.log(MONSTER);
+	
+	
 
 	render();
 }
+
+var interval = setInterval(function(){
+	moveMonster();
+	
+	render();
+
+	if(gameObjects[shipRow][shipColumn] === MONSTER){
+		
+		endGame();
+		
+	}
+}, 500);
+
+
 
 function trade(){
 
@@ -210,11 +261,96 @@ function fight(){
 	}
 }
 
+function moveMonster(){
+
+	var UP = 1;
+	var RIGHT = 2;
+	var DOWN = 3;
+	var LEFT = 4;
+
+	var validDirections = [];
+	var direction;
+
+	if(monsterRow > 0){
+		var thingAbove = map[monsterRow-1][monsterColumn];
+		if(thingAbove === WATER){
+			validDirections.push(UP);
+		}
+	}
+	if(monsterRow<ROWS-1){
+		var thingBelow = map[monsterRow+1][monsterColumn];
+		if(thingBelow === WATER){
+			validDirections.push(DOWN);
+		}
+	}
+	if(monsterColumn>0){
+		var thingLeft = map[monsterRow][monsterColumn-1];
+		if(thingLeft === WATER){
+			validDirections.push(LEFT);
+		}
+	}
+	if(monsterColumn<COLUMNS-1){
+		var thingRight = map[monsterRow][monsterColumn+1];
+		if(thingRight === WATER){
+			validDirections.push(RIGHT);
+		}
+	}
+
+	if(validDirections.length > 0){
+		var direction = validDirections[Math.floor(Math.random()*validDirections.length)];
+	}
+
+	switch (direction) {
+		case UP:
+		
+			gameObjects[monsterRow][monsterColumn] = 0;
+			monsterRow--;
+			gameObjects[monsterRow][monsterColumn] = MONSTER;
+		
+		break;
+		case DOWN:
+		
+			gameObjects[monsterRow][monsterColumn] = 0;
+			monsterRow++;
+			gameObjects[monsterRow][monsterColumn] = MONSTER;
+		
+		break;
+		case RIGHT:
+		
+			gameObjects[monsterRow][monsterColumn] = 0;
+			monsterColumn++;
+			gameObjects[monsterRow][monsterColumn] = MONSTER;
+		
+		break;
+		case LEFT:
+		
+			gameObjects[monsterRow][monsterColumn] = 0;
+			monsterColumn--;
+			gameObjects[monsterRow][monsterColumn] = MONSTER;
+		
+		break;
+	}
+
+
+
+
+	
+
+
+}
+
 function endGame(){
+	
 	if(map[shipRow][shipColumn] === HOME)	{
 		var score = food + gold + experience;
-		gameMessage
-		= "You made it home ALIVE! " + "Final Score: " + score;
+		gameMessage = "You made it home ALIVE! " + "Final Score: " + score;
+	} else if(gameObjects[shipRow][shipColumn] === MONSTER){
+		console.log("elseif");
+		gameObjects[shipRow][shipColumn] = WRECK;
+		
+		gameMessage = " You've been eaten by Monster"
+		render();
+		clearInterval(interval);
 	}else{
 		if(gold <= 0){
 			gameMessage += " You've run out of gold!";
@@ -225,5 +361,6 @@ function endGame(){
 		+= " Your crew throws you overboard!";
 	}
 	document.removeEventListener("keydown", keydownHandler, false);
+	
 }
-
+}
